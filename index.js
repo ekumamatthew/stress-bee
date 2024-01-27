d8a4454117774e26fba517098dbcf22dfac415c4
IkemHood <91676649+ikemHood@users.noreply.github.com>
2024-01-27 11:57:06 +0100

    mongodb connect

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.error('MongoDB connection error:', err));
 
