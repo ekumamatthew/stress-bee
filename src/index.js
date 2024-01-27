97b16db16cd9a638af8e32d2e4250b6b1128cacc
IkemHood <91676649+ikemHood@users.noreply.github.com>
2024-01-27 11:42:25 +0100

    revamp

--- src/index.js
+++ src/index.js
@@ -11,8 +11,8 @@ mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTop
     .catch(err => console.error('MongoDB connection error:', err));
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/participants', require('./routes/participants'));
+app.use('/api/auth', require('./src/routes/auth'));
+app.use('/api/participants', require('./src/routes/participants'));
 app.all('*', (req, res) => {
     res.status(404).json({
         error: `Can't find ${req.originalUrl} on this server!`,
